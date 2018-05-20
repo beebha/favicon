3032f1cc1e05a38181b874c4216db16b9d52fdef
bharathi <bbalasubramanyam@lexialearning.com>
2018-05-20 01:21:06 -0400

    JS enhancements

--- public/js/populate.js
+++ public/js/populate.js
@@ -1,56 +1,76 @@
 var apiUrl = "http://localhost:9000/src/";
 var insertedRecords = 0;
+var processedCount = 0;
+var seedCount = 0;
 var errorCount = 0;
 var allErrorMsg = "";
 
 $(function () {
 
   $('#populateBtn').click(function() {
-    console.log("Populate Button clicked");
     $('#populateBtn').hide();
     populate();
   });
 
   function populate()
   {
-    $('#insertedRowsMsg').html("Processing data file and creating seed files for DB. Patience this will take time...");
+    $('#insertedRowsMsg').html("Processing data file and creating seed files for DB...");
+    createCSVFiles();
+  }
+
+  function createCSVFiles()
+  {
     $.post(
       apiUrl+"app.php?action=createCSVFiles"
     ).done(function(results) {
 
-      var seedCount = results.data.seedCount;
-      var processedCount = seedCount;
+      seedCount = results.data.seedCount;
+      processedCount = seedCount;
 
       $('#insertedRowsMsg').html(seedCount+ " files created for seeding. Preparing for insert into DB...");
 
       for(var i=0; i < seedCount; i++) {
-        $.post(
-          apiUrl+"app.php?action=populateDB",
-          { "seedNumber" : i }
-        ).done(function(results) {
-          if(results.success) {
-            var recordsProcessed = results.data.timeTakenInfo.length - results.error.length;
-            insertedRecords += recordsProcessed;
-            $('#insertedRowsMsg').html(insertedRecords + " rows inserted into DB...");
-          } else {
-            allErrorMsg += results.error[0]+"<br>";
-            $('#errorRowsMsg').html(allErrorMsg);
-            errorCount++;
-          }
-
-          processedCount -=1;
-
-          if(processedCount === 0) {
-            $.post(
-              apiUrl+"app.php?action=deleteFiles",
-              { "fileCount" : seedCount }
-            ).done(function() {
-              $('#insertedRowsMsg').html("");
-              $('#completeMsg').html("A total of " + insertedRecords + " rows were inserted into DB. We. Are. Done!");
-            });
-          }
-        });
+        populateDB(i);
       }
+    });
+  }
+
+  function populateDB(fileNumber)
+  {
+    $.post(
+      apiUrl+"app.php?action=populateDB",
+      { "seedNumber" : fileNumber }
+    ).done(function(results) {
+
+      if(results.success) {
+        var recordsProcessed = results.data.timeTakenInfo.length - results.error.length;
+        insertedRecords += recordsProcessed;
+        $('#insertedRowsMsg').html(insertedRecords + " rows inserted into DB...");
+      } else {
+        allErrorMsg += results.error[0]+"<br>";
+        $('#errorRowsMsg').html(allErrorMsg);
+        errorCount++;
+      }
+
+      processedCount -=1;
+
+      if(processedCount === 0) {
+        deleteFiles();
+      }
+
+    });
+  }
+
+  function deleteFiles()
+  {
+    $.post(
+      apiUrl+"app.php?action=deleteFiles",
+      { "fileCount" : seedCount }
+    ).done(function() {
+
+      $('#insertedRowsMsg').html("");
+      $('#completeMsg').html("A total of " + insertedRecords + " rows were inserted into DB. We. Are. Done!");
+
     });
   }
 });
